d7f70e63cbc1ae4545645b72d7f3182c717adb46
Dai <112484016+Daiivr@users.noreply.github.com>
2023-03-05 19:30:29 -0900

    Update QueueCheckResult.cs

--- SysBot.Pokemon/Queues/QueueCheckResult.cs
+++ SysBot.Pokemon/Queues/QueueCheckResult.cs
@@ -26,12 +26,12 @@ namespace SysBot.Pokemon
         public string GetMessage()
         {
             if (!InQueue || Detail is null)
-                return "You are not in the queue.";
+                return "No estás en la cola.";
             var position = $"{Position}/{QueueCount}";
-            var msg = $"You are in the {Detail.Type} queue! Position: {position} (ID {Detail.Trade.ID})";
+            var msg = $"YEstás en la cola: {Detail.Type}! Posición: {position} (ID {Detail.Trade.ID})";
             var pk = Detail.Trade.TradeData;
             if (pk.Species != 0)
-                msg += $", Receiving: {(Species)Detail.Trade.TradeData.Species}";
+                msg += $", Recibiendo: {(Species)Detail.Trade.TradeData.Species}";
             return msg;
         }
     }
